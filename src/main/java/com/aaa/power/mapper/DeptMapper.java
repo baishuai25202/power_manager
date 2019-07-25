beb35857361dc2d5ce49c540515d54064a3aa89b
bai <252027426@qq.com>
2019-07-25 18:58:45 +0800

    新增加了一个mapper

---
+++ src/main/java/com/aaa/power/mapper/DeptMapper.java
@@ -0,0 +1,16 @@
+package com.aaa.power.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ProjectName: power_manager
+ * @Package: com.aaa.power.mapper
+ * @Author: ${白帅}
+ * @Description: ${description}
+ * @Date: 2019/7/25 18:55
+ * @Version: 1.0
+ */
+public interface DeptMapper {
+    List<Map<String,Object>> getDept();
+}
